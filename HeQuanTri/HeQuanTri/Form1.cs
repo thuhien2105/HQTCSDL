af04f889e3462048e90fd961e96b225248db929d
dthgiang <duongthanhgiang.0108@gmail.com>
2022-11-21 14:46:15 +0700

    first commit - update form login

---
+++ HeQuanTri/HeQuanTri/Form1.cs
@@ -0,0 +1,15 @@
+namespace HeQuanTri
+{
+    public partial class dangnhap : Form
+    {
+        public dangnhap()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
