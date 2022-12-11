1bcec5f9d0187ce7bbb9ea7537fb8c0705caa462
thuhien2105 <thuhien21052002@gmail.com>
2022-12-12 03:47:34 +0700

    update database, chuc nang dang nhap

--- GiaoDien/GiaoDien/Form1.Designer.cs
+++ GiaoDien/GiaoDien/Form1.Designer.cs
@@ -31,108 +31,125 @@ namespace GiaoDien
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.button5 = new System.Windows.Forms.Button();
-            this.label7 = new System.Windows.Forms.Label();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
+            this.lblTenDangNhap = new System.Windows.Forms.Label();
+            this.lblMatKhau = new System.Windows.Forms.Label();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.btnTiepTuc = new System.Windows.Forms.Button();
+            this.lblHoac = new System.Windows.Forms.Label();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // label5
+            // lblTenDangNhap
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(215, 172);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(105, 18);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Tên đăng nhập";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
+            this.lblTenDangNhap.AutoSize = true;
+            this.lblTenDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenDangNhap.Location = new System.Drawing.Point(322, 265);
+            this.lblTenDangNhap.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTenDangNhap.Name = "lblTenDangNhap";
+            this.lblTenDangNhap.Size = new System.Drawing.Size(175, 29);
+            this.lblTenDangNhap.TabIndex = 0;
+            this.lblTenDangNhap.Text = "Tên đăng nhập";
+            this.lblTenDangNhap.Click += new System.EventHandler(this.label5_Click);
             // 
-            // label6
+            // lblMatKhau
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(215, 208);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(69, 18);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Mật khẩu";
+            this.lblMatKhau.AutoSize = true;
+            this.lblMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatKhau.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblMatKhau.Location = new System.Drawing.Point(322, 320);
+            this.lblMatKhau.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(109, 29);
+            this.lblMatKhau.TabIndex = 1;
+            this.lblMatKhau.Text = "Mật khẩu";
+            this.lblMatKhau.Click += new System.EventHandler(this.label6_Click);
             // 
-            // textBox3
+            // txtTenDangNhap
             // 
-            this.textBox3.Location = new System.Drawing.Point(351, 172);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(222, 20);
-            this.textBox3.TabIndex = 2;
+            this.txtTenDangNhap.Location = new System.Drawing.Point(526, 265);
+            this.txtTenDangNhap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(331, 26);
+            this.txtTenDangNhap.TabIndex = 2;
+            this.txtTenDangNhap.TextChanged += new System.EventHandler(this.txtTenDangNhap_TextChanged);
             // 
-            // textBox4
+            // txtMatKhau
             // 
-            this.textBox4.Location = new System.Drawing.Point(351, 208);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(222, 20);
-            this.textBox4.TabIndex = 3;
+            this.txtMatKhau.Location = new System.Drawing.Point(526, 320);
+            this.txtMatKhau.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.PasswordChar = '•';
+            this.txtMatKhau.Size = new System.Drawing.Size(331, 26);
+            this.txtMatKhau.TabIndex = 3;
+            this.txtMatKhau.TextChanged += new System.EventHandler(this.txtMatKhau_TextChanged);
             // 
-            // button5
+            // btnTiepTuc
             // 
-            this.button5.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.button5.Location = new System.Drawing.Point(218, 246);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(355, 28);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Tiếp tục";
-            this.button5.UseVisualStyleBackColor = false;
+            this.btnTiepTuc.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnTiepTuc.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnTiepTuc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTiepTuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTiepTuc.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnTiepTuc.Location = new System.Drawing.Point(327, 378);
+            this.btnTiepTuc.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnTiepTuc.Name = "btnTiepTuc";
+            this.btnTiepTuc.Size = new System.Drawing.Size(532, 43);
+            this.btnTiepTuc.TabIndex = 4;
+            this.btnTiepTuc.Text = "Tiếp tục";
+            this.btnTiepTuc.UseVisualStyleBackColor = false;
+            this.btnTiepTuc.Click += new System.EventHandler(this.btnTiepTuc_Click);
             // 
-            // label7
+            // lblHoac
             // 
-            this.label7.AutoSize = true;
-            this.label7.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label7.Location = new System.Drawing.Point(375, 305);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(37, 13);
-            this.label7.TabIndex = 5;
-            this.label7.Text = "HOẶC";
-            this.label7.Click += new System.EventHandler(this.label7_Click);
+            this.lblHoac.AutoSize = true;
+            this.lblHoac.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblHoac.Location = new System.Drawing.Point(562, 469);
+            this.lblHoac.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHoac.Name = "lblHoac";
+            this.lblHoac.Size = new System.Drawing.Size(55, 20);
+            this.lblHoac.TabIndex = 5;
+            this.lblHoac.Text = "HOẶC";
+            this.lblHoac.Click += new System.EventHandler(this.label7_Click);
             // 
-            // button6
+            // btnDangKy
             // 
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(290, 347);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(100, 33);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "Đăng Ký";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnDangKy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangKy.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangKy.Location = new System.Drawing.Point(435, 534);
+            this.btnDangKy.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(150, 51);
+            this.btnDangKy.TabIndex = 6;
+            this.btnDangKy.Text = "Đăng Ký";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
             // 
-            // button7
+            // btnThoat
             // 
-            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(402, 347);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(100, 33);
-            this.button7.TabIndex = 7;
-            this.button7.Text = "Thoát";
-            this.button7.UseVisualStyleBackColor = true;
+            this.btnThoat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(603, 534);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(150, 51);
+            this.btnThoat.TabIndex = 7;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label8.Location = new System.Drawing.Point(181, 415);
+            this.label8.Location = new System.Drawing.Point(272, 638);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(430, 26);
+            this.label8.Size = new System.Drawing.Size(617, 40);
             this.label8.TabIndex = 8;
             this.label8.Text = "                Chúng tôi không sử dụng thông tin của bạn với bất kỳ mục đích nào" +
     "\r\nBằng cách đăng nhập hoặc đăng ký, bạn đồng ý với Chính sách quy định của chúng" +
@@ -141,32 +158,35 @@ namespace GiaoDien
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(341, 46);
+            this.pictureBox2.Location = new System.Drawing.Point(512, 71);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 89);
+            this.pictureBox2.Size = new System.Drawing.Size(150, 137);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 9;
             this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.lblHoac);
+            this.Controls.Add(this.btnTiepTuc);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTenDangNhap);
+            this.Controls.Add(this.lblMatKhau);
+            this.Controls.Add(this.lblTenDangNhap);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -175,25 +195,16 @@ namespace GiaoDien
 
         #endregion
 
-        private Label label1;
-        private Label label2;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private Label label3;
-        private Label label4;
-        private PictureBox pictureBox1;
-        private Label label5;
-        private Label label6;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private Button button5;
-        private Label label7;
-        private Button button6;
-        private Button button7;
+       
+        //private PictureBox pictureBox1;
+        private Label lblTenDangNhap;
+        private Label lblMatKhau;
+        private TextBox txtTenDangNhap;
+        private TextBox txtMatKhau;
+        private Button btnTiepTuc;
+        private Label lblHoac;
+        private Button btnDangKy;
+        private Button btnThoat;
         private Label label8;
         private PictureBox pictureBox2;
     }
--- GiaoDien/GiaoDien/Form1.cs
+++ GiaoDien/GiaoDien/Form1.cs
@@ -2,27 +2,91 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace GiaoDien
 {
     public partial class Form1 : Form
     {
+        SqlConnection _connection = null;
+        SqlCommand _command = null;
+        String _connectionString = "";
         public Form1()
         {
             InitializeComponent();
+            _connectionString = @"Data Source=MSI\HIENTHU;Initial Catalog=DOAN;Integrated Security=True";
         }
-
         private void label5_Click(object sender, EventArgs e)
         {
 
         }
+        private void label6_Click(object sender, EventArgs e)
+        {
 
+        }
         private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnTiepTuc_Click(object sender, EventArgs e)
+        {
+            String tendangnhap = this.txtTenDangNhap.Text;
+            String matkhau = this.txtMatKhau.Text;
+            //Bước 1: Tạo đối tượng kết nối để CSDL & mở kết nối
+            _connection = new SqlConnection(_connectionString);
+            _connection.Open();
+            //Bước 2: Xây dựng câu lệnh SQL để thực hiện chức năng mong muốn
+            String sql = "SELECT * FROM [TAI KHOAN] where tentaikhoan='"+ tendangnhap +"'";
+            //Bước 3: Tạo đối tượng thực thi câu lệnh
+            _command = new SqlCommand(sql, _connection);
+            _command.Connection = _connection;
+            //Bước 4: Thực thi câu SQL và lấy kết quả trả về
+            SqlDataReader reader = _command.ExecuteReader();
+            if (reader.Read())
+            {
+                MessageBox.Show("Đăng nhập thành công");
+                //string id = reader["Loai tai khoan"].ToString();
+                //Form2 form2 = new Form2();
+                //form2.id_f2 = id;
+                //form2.Show();
+                this.Hide();
+            }
+            else
+                MessageBox.Show("Not found !!!");
+            reader.Close();
+            //Bước 5: Đóng kết nối
+            _connection.Close();
+
+        }
+
+        private void btnDangKy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTenDangNhap_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMatKhau_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
