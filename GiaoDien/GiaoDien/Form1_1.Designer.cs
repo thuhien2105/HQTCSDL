86283287a63dd647d0ee6f947b07b32574aa2ad5
thuhien2105 <thuhien21052002@gmail.com>
2022-12-24 11:38:03 +0700

    chức năng đăng ký

---
+++ GiaoDien/GiaoDien/Form1_1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace GiaoDien
+{
+    partial class Form1_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btndangky = new System.Windows.Forms.Button();
+            this.txttendangnhap = new System.Windows.Forms.TextBox();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
+            this.txthoten = new System.Windows.Forms.TextBox();
+            this.txtsdt = new System.Windows.Forms.TextBox();
+            this.txtdiachi = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TÊN ĐĂNG NHẬP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MẬT KHẨU";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "HỌ TÊN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(216, 329);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "SỐ ĐIỆN THOẠI";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(216, 412);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ĐỊA CHỈ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(216, 488);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "EMAIL";
+            // 
+            // btndangky
+            // 
+            this.btndangky.Location = new System.Drawing.Point(276, 555);
+            this.btndangky.Name = "btndangky";
+            this.btndangky.Size = new System.Drawing.Size(196, 50);
+            this.btndangky.TabIndex = 6;
+            this.btndangky.Text = "ĐĂNG KÝ";
+            this.btndangky.UseVisualStyleBackColor = true;
+            this.btndangky.Click += new System.EventHandler(this.btndangky_Click);
+            // 
+            // txttendangnhap
+            // 
+            this.txttendangnhap.Location = new System.Drawing.Point(438, 98);
+            this.txttendangnhap.Name = "txttendangnhap";
+            this.txttendangnhap.Size = new System.Drawing.Size(100, 26);
+            this.txttendangnhap.TabIndex = 7;
+            // 
+            // txtmatkhau
+            // 
+            this.txtmatkhau.Location = new System.Drawing.Point(438, 169);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.Size = new System.Drawing.Size(100, 26);
+            this.txtmatkhau.TabIndex = 8;
+            // 
+            // txthoten
+            // 
+            this.txthoten.Location = new System.Drawing.Point(438, 249);
+            this.txthoten.Name = "txthoten";
+            this.txthoten.Size = new System.Drawing.Size(100, 26);
+            this.txthoten.TabIndex = 9;
+            // 
+            // txtsdt
+            // 
+            this.txtsdt.Location = new System.Drawing.Point(438, 323);
+            this.txtsdt.Name = "txtsdt";
+            this.txtsdt.Size = new System.Drawing.Size(100, 26);
+            this.txtsdt.TabIndex = 10;
+            // 
+            // txtdiachi
+            // 
+            this.txtdiachi.Location = new System.Drawing.Point(438, 409);
+            this.txtdiachi.Name = "txtdiachi";
+            this.txtdiachi.Size = new System.Drawing.Size(100, 26);
+            this.txtdiachi.TabIndex = 11;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(438, 482);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(100, 26);
+            this.txtemail.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(641, 555);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 50);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "ĐĂNG NHẬP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1_1
+            // 
+            this.AcceptButton = this.btndangky;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 635);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtdiachi);
+            this.Controls.Add(this.txtsdt);
+            this.Controls.Add(this.txthoten);
+            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.txttendangnhap);
+            this.Controls.Add(this.btndangky);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1_1";
+            this.Text = "ĐĂNG KÝ";
+            this.Load += new System.EventHandler(this.Form1_1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btndangky;
+        private System.Windows.Forms.TextBox txttendangnhap;
+        private System.Windows.Forms.TextBox txtmatkhau;
+        private System.Windows.Forms.TextBox txthoten;
+        private System.Windows.Forms.TextBox txtsdt;
+        private System.Windows.Forms.TextBox txtdiachi;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ GiaoDien/GiaoDien/Form1_1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiaoDien
+{
+    public partial class Form1_1 : Form
+    {
+        SqlConnection _connection = null;
+        SqlCommand _command = null;
+        String connectionString = "";
+        public String tendangnhap;
+        public String matkhau;
+        public String hoten;
+        public String sdt;
+        public String diachi;
+        public String email;
+        public Form1_1()
+        {
+            InitializeComponent();
+            connectionString = @"Data Source=MSI\HIENTHU;Initial Catalog=DOAN;Integrated Security=True";
+        }
+
+        private void Form1_1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btndangky_Click(object sender, EventArgs e)
+        {
+            tendangnhap = txttendangnhap.Text;
+            matkhau = txtmatkhau.Text;
+            hoten = txthoten.Text;
+            sdt = txtsdt.Text;
+            diachi = txtdiachi.Text;
+            email = txtemail.Text;
+
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+
+            String sql = "exec sp_signup '" + tendangnhap + "','" + matkhau + "',N'" + hoten + "'," + sdt + ",N'" + diachi + "','" + email + "'";
+            _command = new SqlCommand(sql, _connection);
+            _command.Connection = _connection;
+            int n = _command.ExecuteNonQuery();
+            if (n > 0)
+                MessageBox.Show("Update successfully !!!");
+            else
+                MessageBox.Show("Error !!!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
+            this.Hide();
+        }
+    }
+}
